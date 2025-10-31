90bd379e16aed487858f853fe9bc33c36e0dfbe8
Viet Nguyen <susinguyen.2025@gmail.com>
2025-10-31 21:37:27 +0200

    it could run but not yet check the edge cases

--- src/routine.c
+++ src/routine.c
@@ -1,15 +1,11 @@
 
 #include "philo.h"
-// need to check the start time of philo
-//  skip the odd philo to prevent racing
-//  update last time time
-//  show message thinking
-//  pick fork;
+//lack of check simulation end in each phase
 
-static void pick_fork(t_philo *philo);
-static void think_phase(t_philo *philo);
-static void eat_phase(t_philo *philo);
-static void sleep_phase(t_philo *philo);
+static int lock_fork(t_philo *philo);
+static int think_phase(t_philo *philo);
+static int eat_phase(t_philo *philo);
+static int sleep_phase(t_philo *philo);
 
 void *philo_routine(void *arg)
 {
@@ -24,50 +20,71 @@ void *philo_routine(void *arg)
 	philo->last_meal_time = get_time(); // update the last_meal_time ?? is it correct
 	while (philo->table->end_simu == 0) // if not end_simulation, allow to run
 	{
-		think_phase(philo);
-		pick_fork(philo);
-		eat_phase(philo);
-		sleep_phase(philo);
+		if (lock_fork(philo) == 1)
+			break;
+		if (eat_phase(philo) == 1)
+			break ;
+		if (sleep_phase(philo) == 1)
+			break ;
+		if (think_phase(philo) == 1)
+			break ;
 	}
 	return (NULL);
 }
 
-static void think_phase(t_philo *philo)
+
+static int think_phase(t_philo *philo)
 {
 	print_message(philo->table, philo->id, THINK);
-	safe_usleep(philo->table, philo->table->think_time * 1000);
+	safe_usleep(philo->table, philo->table->think_time);
+	if (philo->table->end_simu == 1)
+		return (1);
+	return (0);
 }
 
-static void pick_fork(t_philo *philo) // not for only 1 philo
+static int lock_fork(t_philo *philo)
 {
-	if (((philo->id - 1) % 2) == 0)
-	{
 		pthread_mutex_lock(philo->left_fork);
+		if (philo->table->end_simu == 1)
+		{
+			pthread_mutex_unlock(philo->left_fork);
+			return (1);
+		}
 		print_message(philo->table, philo->id, FORK_PICK);
 		pthread_mutex_lock(philo->right_fork);
-	}
-	else
-	{
-		pthread_mutex_lock(philo->right_fork);
-		print_message(philo->table, philo->id, FORK_PICK);
-		pthread_mutex_lock(philo->left_fork);
-	}
+		if (philo->table->end_simu == 1)
+		{
+			pthread_mutex_unlock(philo->left_fork);
+			pthread_mutex_unlock(philo->right_fork);
+			return (1);
+		}
+		return (0);
 }
 
-static void eat_phase(t_philo *philo)
+static int eat_phase(t_philo *philo)
 {
+	if (philo->table->end_simu == 1)
+		return (1);
 	philo->last_meal_time = get_time();
 	print_message(philo->table, philo->id, EAT);
-	safe_usleep(philo->table, philo->table->eat_time * 1000);
+	safe_usleep(philo->table, philo->table->eat_time);
+	if (philo->table->end_simu == 1)
+		return (1);
 	philo->have_eaten++;
 	pthread_mutex_unlock(philo->right_fork);
 	pthread_mutex_unlock(philo->left_fork);
+	return (0);
 }
 
-static void sleep_phase(t_philo *philo)
+static int sleep_phase(t_philo *philo)
 {
+	if (philo->table->end_simu == 1)
+		return (1);
 	print_message(philo->table, philo->id, SLEEP);
-	safe_usleep(philo->table, philo->table->sleep_time * 1000);
+	safe_usleep(philo->table, philo->table->sleep_time);
+	if (philo->table->end_simu == 1)
+		return (1);
+	return (0);
 }
 
 void safe_usleep(t_table *table, uint64_t duration)
--- src/simu.c
+++ src/simu.c
@@ -37,13 +37,14 @@ void monitoring(t_table *table)
 	{
 		i = 0;
 		full_count = 0;
-		while (i < table->philo_num && table->end_simu == 0)
+		while (i < table->philo_num)
 		{
 			wait_time = get_time() - table->philos[i].last_meal_time;
 			if (wait_time > table->die_time)
 			{
 				table->end_simu = 1;
 				print_message(table, table->philos[i].id, DIE);
+				break ;
 			}
 			if (table->must_eat != 0 && table->philos[i].have_eaten == table->must_eat)
 				full_count++;
